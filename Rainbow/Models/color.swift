e25da03e928f7659fef610b5e5b1fcb828b2d146
Claudia Maciel <clc_80@yahoo.com>
2020-01-28 19:50:14 -0800

    Skeleton code for tableViewController is set

---
+++ Rainbow/Models/color.swift
@@ -0,0 +1,16 @@
+//
+//  color.swift
+//  Rainbow
+//
+//  Created by Claudia Contreras on 1/28/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+struct MyColor {
+    let name: String
+    let color: UIColor
+}
