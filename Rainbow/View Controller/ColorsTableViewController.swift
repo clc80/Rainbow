8091365b58dc35c7de2494d3e1bf1c4284c74048
Claudia Maciel <clc_80@yahoo.com>
2020-01-28 20:04:00 -0800

    Table View array created and used

--- Rainbow/View Controller/ColorsTableViewController.swift
+++ Rainbow/View Controller/ColorsTableViewController.swift
@@ -9,27 +9,40 @@
 import UIKit
 
 class ColorsTableViewController: UITableViewController {
+    
+    var colors: [MyColor] = [
+        MyColor(name: "Red", color: .red),
+        MyColor(name: "Orange", color: .orange),
+        MyColor(name: "Yellow", color: .yellow),
+        MyColor(name: "Green", color: .green),
+        MyColor(name: "Blue", color: .blue),
+        MyColor(name: "Indigo", color: .cyan),
+        MyColor(name: "Violet", color: .purple)
+        MyColor(name: "Gray", color: .gray)
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return colors.count
     }
 
-    
+    //Index Path (x = section, y = row)
+    //First Row of a tableview (0, 0)
+    //(0,1)
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell", for: indexPath)
-
+        //access the item that was clicked on
+        let color = colors[indexPath.row]
+        
+        // Now we can use the information to set our cell
+        cell.textLabel?.text = color.name
+        cell.backgroundColor = color.color
+        
         return cell
     }
 
